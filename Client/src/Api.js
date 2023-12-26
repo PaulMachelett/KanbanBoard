a3a73930d5ae06a017b21e011b4823e9f4081c5a
Paul Machelett <115557385+PaulMachelett@users.noreply.github.com>
2023-12-26 15:03:20 +0100

    test

--- Client/src/Api.js
+++ Client/src/Api.js
@@ -10,7 +10,7 @@ export default function Api() {
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await fetch(`${baseURL}/kanbancard/phase/1`);
+        const response = await fetch(`${baseURL}/kanbancard/phae/1`);
         if (!response.ok) {
           throw new Error(
             `This is an HTTP error: The status is ${response.status}`
